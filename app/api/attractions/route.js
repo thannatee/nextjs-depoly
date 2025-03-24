57a050ac11aaec7fd876daac8cc4af7c018d000b
Thannatee Tepkumgan <thannatee.t66@rsu.ac.th>
2025-03-24 14:14:57 +0700

    Addapicreate

--- app/api/attractions/route.js
+++ app/api/attractions/route.js
@@ -1,11 +1,28 @@
 import { NextResponse } from "next/server";
 import { mysqlPool } from "@/utils/db";
 
+const db = mysqlPool.promise()
+
 export async function GET(request) {
-  const promisePool = mysqlPool.promise()
-  const [rows, fields] = await promisePool.query(
+  try {
+  const [rows, fields] = await db.query(
     `SELECT * FROM attractions;`
   )
-  return NextResponse.json(rows)
+  return NextResponse.json(rows,{status: 200})
+} catch (error){
+  return NextResponse.json({error:"Failed to fetch"},{status: 500})
+}
+}
+
+export async function POST(request){
+  try {
+      const {name, detail,coverimage, latitude, longitude} = await request.json();
+      const [result] = await db.query(
+          'INSERT INTO attractions (name, detail,coverimage, latitude, longitude) VALUES (?,?,?,?,?)',[name, detail,coverimage, latitude, longitude]
+      );
+      return NextResponse.json({id:result.insertId,name, detail,coverimage, latitude, longitude}, {status: 200});
+  } catch (error){
+      return NextResponse.json({error:error}, {status: 500})
+  }
 }
 
